98d6ade413f61d3dbbf12748525a3492b921f9ac
Masabanda <dimasocol@ukr.net>
2022-04-23 18:24:46 +0300

    init simple components

---
+++ src/components/Header.vue
@@ -0,0 +1,59 @@
+<template>
+<nav class="navbar navbar-expand-sm navbar-dark bg-dark" role="navigation">
+  <div class="container">
+    <!-- Brand and toggle get grouped for better mobile display
+    <router-link to="/" class="navbar-brand mr-auto">StoreSharp</router-link>
+    <div class="collapse navbar-collapse" id="navbarTop" :class="{show: isNavOpen}">
+      <ul class="navbar-nav mr-auto">
+
+      </ul>
+      <ul class="nav navbar-nav">
+        <router-link to="/login" tag="li" v-if="!isLoggedIn" class="nav-item" active-class="active">
+          <a class="nav-link">Login</a>
+        </router-link>
+        <li v-if="isLoggedIn" class="li-pointer nav-item">
+          <span class="nav-link disabled user-select-none">{{ userEmail }}</span>
+        </li>
+        <router-link to="/cart" v-if="isLoggedIn" class="li-pointer nav-item" tag="li">
+          <a class="nav-link">Checkout ($ {{ cartValue }})</a>
+        </router-link>
+        <li v-if="isLoggedIn" class="li-pointer nav-item">
+          <a @click="logout" class="nav-link">Logout</a>
+        </li>
+        <router-link to="/register" tag="li" v-if="!isLoggedIn" class="nav-item" active-class="active">
+          <a class="nav-link">Register</a>
+        </router-link>
+      </ul>
+    </div>
+  </div>
+  /.container -->
+</nav>
+</template>
+
+<script>
+import {
+  mapActions, mapGetters
+} from 'vuex';
+export default {
+  data() {
+    return {
+      isNavOpen: false
+    }
+  },
+}
+</script>
+
+
+<style scoped lange="sass">
+.navbar-btn a {
+  color: white;
+}
+
+.li-pointer {
+  cursor: pointer;
+}
+
+.li-pointer:hover {
+  cursor: pointer;
+}
+</style>
