20133f7bc39e6b554e9fdaaffbc49076764cbd87
Masabanda <dimasocol@ukr.net>
2022-04-26 18:34:57 +0300

    update packages, install all packages, add new functionality to Vuex store

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,20 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
+    browser: true,
+    es6: true
   },
-  'extends': [
-    'plugin:vue/vue3-essential',
+  extends: [
+    'plugin:vue/essential',
     'eslint:recommended'
   ],
-  parserOptions: {
-    parser: '@babel/eslint-parser'
-  },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-console': 'off',
+  },
+  parserOptions: {
+    parser: "babel-eslint",
+    sourceType: 'module',
+    allowImportExportEverywhere: true
   }
-}
+};
